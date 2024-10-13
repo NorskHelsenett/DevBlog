cde962c08a86752c3e6b4b8e189473fd6513bd99
Fredrik Bekkevold <fredrik.bekkevold@nhn.no>
2024-10-13 16:38:09 +0200

    formatting, list and c#

--- ConfrenceMaterials/HelloStavander2024/source/fileshare_web/SecretFile.cs
+++ ConfrenceMaterials/HelloStavander2024/source/fileshare_web/SecretFile.cs
@@ -3,9 +3,9 @@ public record SecretFile
     public required Guid Id { get; set; }
     public required string Name { get; set; }
 
-public required string Size {get;set;}
-public required FileRights Rights {get;set;}
-public bool Delete => (Rights & FileRights.Owner) == 0;
-public bool Download => (Rights & (FileRights.Owner | FileRights.Shared)) == 0;
-public bool Share => (Rights & FileRights.Owner) == 0;
+    public required string Size { get; set; }
+    public required FileRights Rights { get; set; }
+    public bool Delete => (Rights & FileRights.Owner) == 0;
+    public bool Download => (Rights & (FileRights.Owner | FileRights.Shared)) == 0;
+    public bool Share => (Rights & FileRights.Owner) == 0;
 }
