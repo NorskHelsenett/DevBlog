9ad46de7410b6b7790456d24f9162e3499597588
Simon Randby <Simon.Glisic.Randby@nhn.no>
2024-10-14 10:47:28 +0200

    HelloStavanger2024 Blazor: Add service for retrieving list of files

---
+++ ConfrenceMaterials/HelloStavander2024/source/fileshare_web/KafkaFileApiService.cs
@@ -0,0 +1,25 @@
+namespace fileshare_web;
+
+public class KafkaFileApiService(HttpClient httpClient)
+{
+    public async Task<List<string>> GetListOfFiles(string accessToken)
+    {
+        var address = $"{Environment.GetEnvironmentVariable("FILESHARE_WEB_REMOTE_FILE_API_ADDRESS")}/list";
+        var request = new HttpRequestMessage(HttpMethod.Get, new Uri(address));
+        request.Headers.Add("Authorization", $"Bearer {accessToken}");
+        try
+        {
+            var response = await httpClient.SendAsync(request);
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseItems = responseString.Split('\n');
+            return responseItems.ToList();
+        }
+        catch (Exception e) {
+            Console.WriteLine("\n\nRequest to external service failed");
+            Console.WriteLine(e);
+            // return $"Stack: {e.StackTrace}"; // Don't do this at home
+        }
+
+        return [];
+    }
+}
