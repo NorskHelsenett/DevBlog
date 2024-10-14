8d2306ecef090039340e280b05f555fc4e659bf8
Simon Randby <Simon.Glisic.Randby@nhn.no>
2024-10-14 09:29:31 +0200

    HelloStavanger2024 API: Fix bearer scheme

--- ConfrenceMaterials/HelloStavander2024/source/fileshare_rest/Program.cs
+++ ConfrenceMaterials/HelloStavander2024/source/fileshare_rest/Program.cs
@@ -1,7 +1,4 @@
 global using static EnvVarNames;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -26,52 +23,38 @@ else
 }
 
 builder.Services.AddSingleton(httpClient);
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
-    {
-        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+builder.Services.AddAuthentication().AddJwtBearer("OurBearerScheme", options =>
+{
+     var backendIdpUrl =
+         Environment.GetEnvironmentVariable(
+             "OIDC_IDP_ADDRESS_FOR_SERVER"); // "http://keycloak:8088/realms/lokalmaskin"
+     var clientIdpUrl =
+         Environment.GetEnvironmentVariable(
+             "OIDC_IDP_ADDRESS_FOR_USERS"); // "http://localhost:8088/realms/lokalmaskin"
+     options.Configuration = new()
+     {
+         Issuer = backendIdpUrl,
+         AuthorizationEndpoint = $"{clientIdpUrl}/protocol/openid-connect/auth",
+         TokenEndpoint = $"{backendIdpUrl}/protocol/openid-connect/token",
+         JwksUri = $"{backendIdpUrl}/protocol/openid-connect/certs",
+         JsonWebKeySet = FetchJwks($"{backendIdpUrl}/protocol/openid-connect/certs"),
+         EndSessionEndpoint = $"{clientIdpUrl}/protocol/openid-connect/logout",
+     };
+     Console.WriteLine("Jwks: " + options.Configuration.JsonWebKeySet);
+     foreach (var key in options.Configuration.JsonWebKeySet.GetSigningKeys())
+     {
+         options.Configuration.SigningKeys.Add(key);
+         Console.WriteLine("Added SigningKey: " + key.KeyId);
+     }
 
-        var backendIdpUrl =
-            Environment.GetEnvironmentVariable(
-                "OIDC_IDP_ADDRESS_FOR_SERVER"); // "http://keycloak:8088/realms/lokalmaskin"
-        var clientIdpUrl =
-            Environment.GetEnvironmentVariable(
-                "OIDC_IDP_ADDRESS_FOR_USERS"); // "http://localhost:8088/realms/lokalmaskin"
-
-        options.Configuration = new()
-        {
-            Issuer = backendIdpUrl,
-            AuthorizationEndpoint = $"{clientIdpUrl}/protocol/openid-connect/auth",
-            TokenEndpoint = $"{backendIdpUrl}/protocol/openid-connect/token",
-            JwksUri = $"{backendIdpUrl}/protocol/openid-connect/certs",
-            JsonWebKeySet = FetchJwks($"{backendIdpUrl}/protocol/openid-connect/certs"),
-            EndSessionEndpoint = $"{clientIdpUrl}/protocol/openid-connect/logout",
-        };
-        Console.WriteLine("Jwks: " + options.Configuration.JsonWebKeySet);
-        foreach (var key in options.Configuration.JsonWebKeySet.GetSigningKeys())
-        {
-            options.Configuration.SigningKeys.Add(key);
-            Console.WriteLine("Added SigningKey: " + key.KeyId);
-        }
-
-        options.ClientId = Environment.GetEnvironmentVariable("OIDC_CLIENT_ID"); // "my_app"
-
-        options.TokenValidationParameters.ValidIssuers = [clientIdpUrl, backendIdpUrl];
-        options.TokenValidationParameters.NameClaimType = "name"; // This is what populates @context.User.Identity?.Name
-        options.TokenValidationParameters.RoleClaimType = "role";
-        options.RequireHttpsMetadata =
-            Environment.GetEnvironmentVariable("OIDC_REQUIRE_HTTPS_METADATA") != "false"; // disable only in dev env
-        options.ResponseType = OpenIdConnectResponseType.Code;
-        options.GetClaimsFromUserInfoEndpoint = true;
-        options.SaveTokens = true;
-        options.MapInboundClaims = true;
-
-        // options.Scope.Clear();
-        options.Scope.Add("openid");
-        options.Scope.Add("profile");
-        options.Scope.Add("email");
-    })
-    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+     options.TokenValidationParameters.ValidIssuers = [clientIdpUrl, backendIdpUrl];
+     options.TokenValidationParameters.NameClaimType = "name"; // This is what populates @context.User.Identity?.Name
+     options.TokenValidationParameters.RoleClaimType = "role";
+     options.RequireHttpsMetadata =
+         Environment.GetEnvironmentVariable("OIDC_REQUIRE_HTTPS_METADATA") != "false"; // disable only in dev env
+     options.MapInboundClaims = true;
+     options.Audience = Environment.GetEnvironmentVariable("OIDC_AUDIENCE");
+});
 
 JsonWebKeySet FetchJwks(string url)
 {
@@ -88,9 +71,6 @@ JsonWebKeySet FetchJwks(string url)
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorization();
-
-// builder.Services.AddAuthentication().AddJwtBearer();
-// builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
