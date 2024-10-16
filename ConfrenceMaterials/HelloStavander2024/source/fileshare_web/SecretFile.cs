3533a063ede11e294054a24ee3bf43b3d48209b8
Fredrik Bekkevold <fredrik.bekkevold@nhn.no>
2024-10-16 17:38:14 +0200

    download working?

--- ConfrenceMaterials/HelloStavander2024/source/fileshare_web/SecretFile.cs
+++ ConfrenceMaterials/HelloStavander2024/source/fileshare_web/SecretFile.cs
@@ -1,6 +1,7 @@
 public record SecretFile
 {
     public required string Name { get; set; }
+    public required string OwnerName {get; set;}
     public required FileRights Rights { get; set; }
     public bool Delete => (Rights & FileRights.Owner) == 0;
     public bool Download => (Rights & (FileRights.Owner | FileRights.Shared)) == 0;
