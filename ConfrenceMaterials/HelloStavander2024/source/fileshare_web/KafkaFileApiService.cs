80f86620167b5219aeb5a2a996c312e55ffb451c
Simon Randby <Simon.Glisic.Randby@nhn.no>
2024-10-16 17:42:56 +0200

    Fix download file request

--- ConfrenceMaterials/HelloStavander2024/source/fileshare_web/KafkaFileApiService.cs
+++ ConfrenceMaterials/HelloStavander2024/source/fileshare_web/KafkaFileApiService.cs
@@ -9,24 +9,24 @@ public class KafkaFileApiService(HttpClient httpClient, ILogger<KafkaFileApiServ
     {
 
         var address = $"{Environment.GetEnvironmentVariable("FILESHARE_WEB_REMOTE_FILE_API_ADDRESS")}/retrieve";
-        var request = new HttpRequestMessage(HttpMethod.Post, new Uri(address));
+        var request = new HttpRequestMessage(HttpMethod.Get, new Uri(address));
         request.Headers.Add("X-Blob-Name", fileName);
         request.Headers.Add("X-Owner-id", owner);
         request.Headers.Add("Authorization", $"Bearer {accessToken}");
         try
         {
             var response = await httpClient.SendAsync(request);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
             {
-                return await response.Content.ReadAsStreamAsync();
+                throw new Exception("Failed to get file");
             }
+            return await response.Content.ReadAsStreamAsync();
         }
         catch (Exception e)
         {
             logger.LogError(e, "\n\nRequest to external service failed");
             throw;
         }
-        throw new Exception("Failed to get file");
     }
     public async Task<bool> DeleteFile(string accessToken, string fileName)
     {
