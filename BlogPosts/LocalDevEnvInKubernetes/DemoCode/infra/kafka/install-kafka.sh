60fa4f5a308d655b9d714b77a296f31ac676e0be
Simon Randby <Simon.Glisic.Randby@nhn.no>
2026-08-12 16:01:11 +0200

    BlogPost LocalKubernetes: Add kafka UI

--- BlogPosts/LocalDevEnvInKubernetes/DemoCode/infra/kafka/install-kafka.sh
+++ BlogPosts/LocalDevEnvInKubernetes/DemoCode/infra/kafka/install-kafka.sh
@@ -3,6 +3,7 @@
 a="/$0"; a="${a%/*}"; a="${a:-.}"; a="${a##/}/"; SCRIPT_DIR=$(cd "$a"; pwd)
 
 KAFKA_NS=kafka
+RELEASE_NAME_KAFBAT_UI=kafbat-ui
 
 echo "🪣 Create namespace for kafka"
 kubectl apply -f - >/dev/null <<EOF
@@ -43,6 +44,36 @@ kubectl apply -f "$SCRIPT_DIR/manifest-kafka-schema-registry.yaml" -n kafka 1>/d
 echo "⏳ Waiting for schema registry to be ready"
 kubectl -n $KAFKA_NS wait --for=condition=available deployments/schema-registry --timeout=20s 1>/dev/null
 
+echo "🌐 Schema registry: http://schema-registry.localho.st:8080/apis/ccompat/v7"
+
+echo "🧑‍🎨 Setting up Kafbat UI: Adding helm repo"
+helm repo add kafbat-ui https://kafbat.github.io/helm-charts 1>/dev/null
+echo "🧑‍🎨 Setting up Kafbat UI: Updating helm repo"
+helm repo update 1>/dev/null
+echo "🧑‍🎨 Setting up Kafbat UI: Install chart using config from map"
+helm install $RELEASE_NAME_KAFBAT_UI kafbat-ui/kafka-ui \
+  --namespace $KAFKA_NS \
+  -f "$SCRIPT_DIR/values-kafbat-ui.yaml"
+kubectl apply -f - >/dev/null <<EOF
+apiVersion: gateway.networking.k8s.io/v1
+kind: HTTPRoute
+metadata:
+  name: hr-kafbat-ui
+  namespace: $KAFKA_NS
+spec:
+  parentRefs:
+  - name: ourgateway
+    namespace: networking-gateway
+    kind: Gateway
+  hostnames:
+  - kafka-ui.localho.st
+  - kafka-ui.127.0.0.1.nip.io
+  rules:
+  - backendRefs:
+    - name: $RELEASE_NAME_KAFBAT_UI-kafka-ui
+      port: 80
+EOF
+echo "🌐 Kafka UI: http://kafka-ui.localho.st:8080"
 
 # kubectl -n kafka run kafka-producer -ti --image=quay.io/strimzi/kafka:1.1.0-kafka-4.3.0 --rm=true --restart=Never -- bin/kafka-console-producer.sh --bootstrap-server my-cluster-kafka-bootstrap:9092 --topic my-topic
 
